e9b75b171e7edf9c593575b6b579aed606a8915c
YashrajsinhChauhan <YashrajChauhan09@gmail.com>
2024-01-15 22:51:11 -0400

    LightThemeUpdate

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,20 @@ contactFromItems.forEach((item) => {
     })
 })
 
+function toggleMode() {
+    let theme = document.querySelector('html').getAttribute('theme');
+
+    if(theme == "dark") {
+        theme = "light";
+        new Audio('./assets/audio/light.wav').play()
+    } else {
+        theme = "dark";
+        new Audio('./assets/audio/dark.wav').play()
+    }
+
+    document.querySelector('html').setAttribute("theme", theme)
+}
+
 // let contactFromSubmit = document.querySelector('#contact_me .form button');
 
 // contactFromSubmit.addEventListener('click', function onClick() {
